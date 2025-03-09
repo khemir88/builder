4df6cb10645c276960a3fbb808b29e112196a7a3
Cristian T <cristian_t1@hotmail.com>
2025-03-09 13:32:35 -0600

    Agregada nueva funcionalidad en bra1

--- centos.sh
+++ centos.sh
@@ -1,15 +1,15 @@
-dic_file="/root/rancho.txt"
+dic_file="gr2.txt"
 random_word=$(shuf -n 1 "$dic_file")
 echo "$random_word"
 
 virt-install \
-	--name $random_word --memory 2048 \
+	--name $random_word.qcow --memory 2048 \
 	--disk $random_word,size=6.1 \
 	--os-variant centos7.0 \
 	--memory 4098 \
 	--vcpus 2 \
 	--graphics none \
-	--location location=/mnt/mucho2/CentOS-Stream-8-x86_64-latest-dvd1.iso \
+	--location location=../isos/CentOS-7-x86_64-DVD-2207-02.iso \
 	--extra-args 'console=ttyS0,115200n8 serial' \
 	--initrd-inject=centos7.cfg \
 	--extra-args inst.ks=file:centos7.cfg \
